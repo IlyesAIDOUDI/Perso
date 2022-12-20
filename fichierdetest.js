98c167d3bc16b1f444cf0348a6a665dc1e3f968e
Ilyes Aïdoudi <ilyes.aidoudi@hotmail.com>
2022-12-20 17:24:09 +0100

    commentaire

--- fichierdetest.js
+++ fichierdetest.js
@@ -1,11 +1,9 @@
-function OnPrinterStateChanged(state) {
-    var evt = new CustomEvent('printerstatechanged', { detail: state });
+//On sélectionne le premier button et le premier div du document
+let b1 = document.querySelector('button');
+let d1 = document.querySelector('div');
 
-    window.dispatchEvent(evt);
-}
-
-
-//Listen to your custom event
-window.addEventListener('printerstatechanged', function (e) {
-    console.log('printer state changed', e.detail);
-});
+//On utilise la méthode addEventListener pour gérer des évènements
+b1.addEventListener('click', function () { alert('Bouton cliqué') });
+d1.addEventListener('mouseover', function () { this.style.backgroundColor = 'orange' });
+d1.addEventListener('mouseover', function () { this.style.fontWeight = 'bold' });
+d1.addEventListener('mouseout', function () { this.style.backgroundColor = 'white' });
